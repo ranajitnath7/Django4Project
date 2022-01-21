97317234cddd0a14ac56bb9d4b4babdde01e1ff1
Ranajit Nath <ranajitnath7@gmail.com>
2022-01-21 22:07:00 +0530

    health path added in urls.py

--- Django4Project/urls.py
+++ Django4Project/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import http
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    path('health', lambda req: http.HttpResponse(200)),
     path('admin/', admin.site.urls),
 ]
